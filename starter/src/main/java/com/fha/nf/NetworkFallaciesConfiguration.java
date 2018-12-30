7c00db31f1ba437e6d737178ecc34cf857009384
Fouad <1621862+fouadh@users.noreply.github.com>
2018-12-30 17:59:37 +0100

    Update NetworkFallaciesConfiguration.java

--- starter/src/main/java/com/fha/nf/NetworkFallaciesConfiguration.java
+++ starter/src/main/java/com/fha/nf/NetworkFallaciesConfiguration.java
@@ -1,10 +1,8 @@
 package com.fha.nf;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@Configuration
 @EnableAspectJAutoProxy
 public class NetworkFallaciesConfiguration {
   @Bean
